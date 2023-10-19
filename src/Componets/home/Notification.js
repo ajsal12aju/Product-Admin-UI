1fb9566da044b880fa8365138a22ca5c30986ac8
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-19 23:47:41 +0530

    updated

--- src/Componets/home/Notification.js
+++ src/Componets/home/Notification.js
@@ -30,10 +30,10 @@ function Notification() {
                 <img className='rounded-circle' src={notification?.pic} alt="" />
               </div>
               <div className='media-body'>
-                <p className='mb-2'> &nbsp; &nbsp; &nbsp; <b>{notification?.message}</b> and <b>Sent you</b> 
+                <p className='mb-2 ml-5 pl-5'><b>{notification?.message}</b> and <b>Sent you</b> 
                  <a style={{textDecoration:"none"}} href="" className='tm-notification-link'>  Products updates</a>.
                 </p>
-                <span className='tm-small tm-text-color-secondary'> &nbsp; &nbsp; &nbsp; 6h ago</span>
+                <span className='tm-small tm-text-color-secondary'>{notification?.time}</span>
               </div>
             </div>
             ))}
