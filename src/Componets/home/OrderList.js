b72881cb9b79b611c938c6b4168815f5a9ab7267
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-20 07:36:42 +0530

    updated

--- src/Componets/home/OrderList.js
+++ src/Componets/home/OrderList.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import './home.css'
 
 function YourComponent() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    
+   
     fetch('https://reactmusicplayer-ab9e4.firebaseio.com/project-data.json')
       .then((response) => response.json())
-      .then((data) => setData(data.orders))
+      .then((data) => setData(data?.dasbhoardPage?.orders))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
+  console.log(data, "data")
+
   return (
-    <table>
-      <thead>
+    <div className="tm-bg-primary-dark tm-block tm-block-taller tm-block-scroll">
+    <table className='table1'>
+      <thead  className='table-head'>
         <tr>
-          <th>ORDER NO</th>
+          <th style={{padding:"20px"}}>ORDER NO</th>
           <th>STATUS</th>
           <th>OPERATORS</th>
           <th>LOCATION</th>
@@ -25,19 +29,26 @@ function YourComponent() {
         </tr>
       </thead>
       <tbody>
-        {data.map((order) => (
-          <tr key={order.orderNo}>
-            <td>{order.orderNo}</td>
-            <td>{order.status}</td>
-            <td>{order.operators}</td>
-            <td>{order.location}</td>
-            <td>{order.distance}</td>
-            <td>{order.startDate}</td>
-            <td>{order.deliveryDate}</td>
+        {data && data.length > 0 ? (
+          data.map((order) => (
+            <tr key={order.orderNo}>
+              <td style={{padding:"20px"}}>{order.orderNo}</td>
+              <td>{order.status}</td>
+              <td>{order.operators}</td>
+              <td>{order.location}</td>
+              <td>{order.distance}</td>
+              <td>{order.startDate}</td>
+              <td>{order.deliveryDate}</td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan="7">Loading...</td>
           </tr>
-        ))}
+        )}
       </tbody>
     </table>
+    </div>
   );
 }
 
