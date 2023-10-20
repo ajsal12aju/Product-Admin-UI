37e49156fefdbb3d3e6c69127bf131c817b2d722
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-20 20:45:18 +0530

    updated

--- src/Componets/products/products.js
+++ src/Componets/products/products.js
@@ -18,11 +18,11 @@ function Products() {
 
   return (
     <div className='container mt-5'>
-      <div className="row tm-content-row">
+      <div className="row justify-content-center tm-content-row">
         <div className="col-sm-12 col-md-12 col-lg-8 col-xl-8 tm-block-col">
             <div className='tm-bg-primary-dark tm-block tm-block-products'>
                 <div className='tm-product-table-container'>
-                    <table className='table table-hover tm-table-small tm-product-table'>
+                    <table className='table1 table-hover tm-table-small tm-product-table'>
                         <thead>
                             <tr>
                                 <th scope="col">PRODUCT NAME</th>
@@ -30,13 +30,14 @@ function Products() {
                                 <th scope="col">UNIT SOLD</th>
                                 <th scope="col">IN STOCK</th>
                                 <th scope="col">EXPIRE DATE</th>
+                                <th></th>
                                
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                <th scope="row"><input type="checkbox"/></th>
-                                <td>sdefdg</td>
+                                <td> sdefdg</td>
                                 <td>sdefdg</td>
                                 <td>sdefdg</td>
                                 <td>sdefdg</td>
@@ -48,6 +49,31 @@ function Products() {
                         </tbody>
                     </table>
                 </div>
+                <a href="add-product.html" class="btn btn-primary btn-block text-uppercase mb-3">Add new product</a>
+                <a href="add-product.html" class="btn btn-primary btn-block text-uppercase mb-3">Delete selected Products</a>
+            </div>
+        </div>
+        <div className="col-sm-12 col-md-12 col-lg-3 col-xl-3 tm-block-col">
+            <div className='tm-bg-primary-dark tm-block tm-block-products'>
+                <div className='tm-product-table-container'>
+                    <table className='table1 table-hover tm-table-small tm-product-table'>
+                        <thead>
+                            <tr>
+                              
+                                <th scope="col">CATEGORY</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                               
+                                <td> sdefdg</td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <a href="add-product.html" class="btn btn-primary btn-block text-uppercase mb-3">Add new Category</a>
             </div>
         </div>
       </div>
