4e5cd031b4a0046db4fb4fb9dddeee87c8feb0d5
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-11-27 20:53:11 +0530

    updated

--- src/Componets/home/Home.js
+++ src/Componets/home/Home.js
@@ -89,7 +89,7 @@ function Home() {
      
         <div className='col-sm-12 col-md-12 col-lg-6'>
           <div className='card-style'>
-          <h2 style={{marginLeft:"20px"}} className='tm-block-title'>Latest Hits</h2>
+          <h2 style={{marginLeft:"20px"}} className='tm-block-title'>Updated Hits</h2>
             <ReactApexChart
               className="chart-container"
               options={chartData.options}
